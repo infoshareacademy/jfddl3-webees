84daf84b6436e49a9e6712e0c80dfd54bdeb2890
Grzegorz <rc36@wp.pl>
2017-12-06 16:39:15 +0100

    Add sections: team, our photo, border radius on foto and photo efects.Add also foto description.Neeed ours foto

---
+++ 1.js
@@ -0,0 +1,21 @@
+var nullVar = null
+var undefinedVar = undefined
+var numberVar = 1
+var boolVar = true
+var stringVar = "ala"
+var arrayVar = [1,2,3]
+var objVar ={a:1, b:2}
+
+
+var types .warn[
+var nullVar,
+var undefinedVar,
+var numberVar,
+var boolVar,
+var stringVar,
+var arrayVar,
+var objVar
+]
+
+
+console.log(typesArray)
