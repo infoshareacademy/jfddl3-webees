5fe20d0a96ba398ae7a8d78f92dc4a3eb0e7991f
Pawel Lorencowicz <p.lorencowicz@gmail.com>
2017-12-14 17:32:47 +0100

    added button to scroll to top

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
-function cookies(){
+function displayCookies(){
     if(localStorage.getItem('cookies') !== 'set') {
-        localStorage.setItem('cookies', 'set');
         var cookiesBox = document.createElement("div");
         cookiesBox.setAttribute('id', 'cookiesBox');
         document.body.appendChild(cookiesBox);
-        cookiesBox.innerHTML = "<div><h4>Strona wykorzystuje pliki cookies.</h4><h4 id='closeCookies'>Zamknij</h4></div><p>W ramach naszej witryny stosujemy pliki cookies w celu świadczenia Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień dotyczących cookies oznacza, że będą one zamieszczane w Państwa urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany ustawień dotyczących cookies.</p>"
+        cookiesBox.innerHTML = "<div><h4>Strona wykorzystuje pliki cookies.</h4><h4 id='acceptCookies'>Akceptuję</h4></div><p>W ramach naszej witryny stosujemy pliki cookies w celu świadczenia Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień dotyczących cookies oznacza, że będą one zamieszczane w Państwa urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany ustawień dotyczących cookies.</p>"
 
-        closeCookies.addEventListener('click',function(){cookiesBox.remove()})
+        acceptCookies.addEventListener('click',function(){
+            localStorage.setItem('cookies', 'set');
+            cookiesBox.remove()
+        })
     }
 }
+
+function moveToTopButtonDisplay(){
+    if(window.pageYOffset !== 0)
+        moveToTopButton.style.display = ""
+    else
+        moveToTopButton.style.display = "none"
+}
+
+function scrollToTop(){
+    setTimeout(function(){
+        window.scrollTo(0, window.pageYOffset-20)
+        if(window.pageYOffset>0)
+            scrollToTop()
+    }, 1)
+}
+
+
+// event window on scroll
+// staly czas przeskoku. rozna predkosc przesuwu
